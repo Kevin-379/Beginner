4b7cf7f39e9a7ffeebcfac903e3a168ada8ef0fb
Kevin Shah <kevin2252238@gmail.com>
2019-10-30 20:54:23 +0530

    weighted uniform string(final)

--- Project1/temp.py
+++ Project1/temp.py
@@ -1,81 +1,27 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-
-class Linkedlist():
-    def __init__(self):
-        self.head = None
-
-    def add(self, data):
-        temp = Node(data)
-        temp.next = self.head
-        self.head = temp
-
-    def find(self, item):
-        current = self.head
-        found = False
-        while current is not None:
-            if current.data == item:
-                found = True
-                break
-            else:
-                current = current.next
-        if found:
-            print("item is present in list")
+# sets are very, very fast for finding element in it w.r.t list. (>10sec to <0.3sec)
+def weightedUniformStrings(s, queries):
+    su = set()
+    prev = -1
+    length = 1
+    for c in s:
+        w = ord(c)-ord('a')+1
+        if c != prev:
+            su.add(w)
+            length = 1
         else:
-            print("item is not present in the list")
-
-    def remove(self, item):
-        current = self.head
-        previous = None
-        found = False
-        while current is not None:
-            if current.data == item:
-                found = True
-                break
-            else:
-                previous = current
-                current = current.next
-        if found:
-            previous.next = current.next
+            length += 1
+            su.add(w*length)
+        prev = c
+    t = []
+    for i in queries:
+        if i in su:
+            t.append('Yes')
         else:
-            print("item is not present in the list")
+            t.append('No')
+    return t
 
-    def printlist(self):
-        l="["
-        current = self.head
-        while current.next is not None:
-            l+=str(current.data)+","
-            current = current.next
-        l+=str(current.data)
-        l+="]"
-        print(l)
 
-    def length(self):
-        count = 0
-        current = self.head
-        while current is not None:
-            count += 1
-            current = current.next
-        print(count)
+print(weightedUniformStrings('abbc', [5, 1, 4, 3]))
+print(weightedUniformStrings('ahasfaa', [5, 19, 6, 1, 2]))
 
-    def append(self, data):
-        temp = Node(None)
-        temp.next = self.head
-        self.head = temp
-        current = self.head
-        while current.next is not None:
-            current.data = current.next.data
-            current = current.next
-        current.data = data
-
-linkedlist = Linkedlist()
-linkedlist.add(4)
-linkedlist.add(1)
-linkedlist.length()
-linkedlist.add(5)
-linkedlist.remove(1)
-linkedlist.append(1)
-linkedlist.printlist()
+s=set()
