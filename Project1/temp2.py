a8c7138f768581efa812de17ab7389089a6d5049
Kevin Shah <kevin2252238@gmail.com>
2019-10-30 17:35:05 +0530

    longest common subsequence(correct)

--- Project1/temp2.py
+++ Project1/temp2.py
@@ -34,7 +34,7 @@ def longestcommonsubsequence(a, b):
         j = 1
     i = len(a) - 1
     j = len(b) - 1
-    s = ''
+    s = []
     while i > 0 and j > 0:
         if lcs[i][j] == 0:
             break
@@ -43,17 +43,32 @@ def longestcommonsubsequence(a, b):
         elif lcs[i][j] == lcs[i - 1][j]:
             i = i - 1
         else:
-            s = s + a[i]
+            s.append(a[i])
             i = i - 1
             j = j - 1
-    if (i == 0 or j == 0) and i != j:
-        if lcs[0][0] != lcs[i][j]:
-            s = s + a[i]
-    if lcs[0][0] == 1:
-        s = s + a[0]
+    if lcs[i][j] == 1:
+        if j == 0 and i != 0:
+            while i >= 0:
+                if lcs[i][0] == 0:
+                    s.append(a[i + 1])
+                    break
+                i = i - 1
+        elif i == 0 and j != 0:
+            while j >= 0:
+                if lcs[0][j] == 0:
+                    s.append(b[j + 1])
+                    break
+                j = j - 1
+        else:
+            s.append(a[0])
     return s[::-1]
 
 
 print(longestcommonsubsequence('abazdc', 'bacbad'))
 print(longestcommonsubsequence('abcdaf', 'acbcf'))
 print(longestcommonsubsequence('abcdef', 'xyzabc'))
+a = [16, 27, 89, 79, 60, 76, 24, 88, 55, 94, 57, 42, 56, 74, 24, 95, 55, 33, 69, 29, 14, 7, 94, 41, 8, 71, 12, 15, 43,
+     3, 23, 49, 84, 78, 73, 63, 5, 46, 98, 26, 40, 76, 41, 89, 24, 20, 68, 14, 88, 26]
+b = [27, 76, 88, 0, 55, 99, 94, 70, 34, 42, 31, 47, 56, 74, 69, 46, 93, 88, 89, 7, 94, 41, 68, 37, 8, 71, 57, 15, 43,
+     89, 43, 3, 23, 35, 49, 38, 84, 98, 47, 89, 73, 24, 20, 14, 88, 75]
+print(longestcommonsubsequence(a, b))
