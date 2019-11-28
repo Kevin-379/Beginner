f9065af3a81d91a59a261cf0f4970c4daa3ec17a
Kevin Shah <kevin2252238@gmail.com>
2019-11-28 13:20:14 +0530

    Learning pygame #1

--- Project1/learningPygame.py
+++ Project1/learningPygame.py
@@ -1,25 +1,46 @@
 # Creating an empty screen that can be closed
 import pygame
+# initializing pygame
+pygame.init()
 
+# creating a screen
+screen = pygame.display.set_mode((240, 180))
 
-def main():
-    # initializing pygame
-    pygame.init()
+# loading and setting logo and title
+#logo = pygame.image.load("logo32x32.png")
+#pygame.display.set_icon(logo)
+pygame.display.set_caption("First pygame")
 
-    # loading and setting logo and title
-    #logo = pygame.image.load("logo32x32.png")
-    #pygame.display.set_icon(logo)
-    pygame.display.set_caption("First pygame")
+x = 50
+y = 50
+h = 20
+w = 20
+v = 5
 
-    # creating a screen
-    screen = pygame.display.set_mode((240, 180))
-    running = True
+running = True
+while running:
+    pygame.time.delay(100)
 
-    while running:
-        for event in pygame.event.get():
-            # adding option to quit anytime
-            if event.type == pygame.QUIT:
-                running = False
+    for event in pygame.event.get():
+        # adding option to quit anytime
+        if event.type == pygame.QUIT:
+            running = False
 
+    keys = pygame.key.get_pressed()
 
-main()
+    if keys[pygame.K_LEFT]:
+        x -= v
+    if keys[pygame.K_RIGHT]:
+        x += v
+    if keys[pygame.K_UP]:
+        y -= v
+    if keys[pygame.K_DOWN]:
+        y += v
+
+    screen.fill((0, 0, 0))
+    pygame.draw.rect(screen, (255, 0, 0), (x, y, h, w))
+    pygame.display.update()
+
+# uninitialize pygame
+# pygame closes automatically when interpreter stops
+pygame.quit()
