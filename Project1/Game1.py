0d40d88828f5973226189c23171ca04c0d06d824
Kevin Shah <kevin2252238@gmail.com>
2019-11-28 16:19:41 +0530

    Pong multiplayer V1.1

--- Project1/Game1.py
+++ Project1/Game1.py
@@ -17,6 +17,9 @@ r = int(min(sh, sw)/50)
 v = 5
 ux = 5
 uy = 5
+score1 = 0
+score2 = 0
+font = pygame.font.SysFont('comicsans', 30)
 
 running = True
 while running:
@@ -38,25 +41,50 @@ while running:
     if keys[pygame.K_DOWN] and y2 <= sh-h-v:
         y2 += v
 
+    V = True
+
     if x+r+ux > x2 and y2 <= y+uy <= y2+h:
         ux = -ux
     elif x-r+ux < x1+w and y1 <= y+uy <= y1+h:
         ux = -ux
-    elif x+r+ux > sw:
-        ux = -ux
-    elif x-r+ux < 0:
-        ux = -ux
-    if y-r+uy < 0:
+    elif x+r+ux > x2:
+        score1 += 1
+        x1 = 0
+        y1 = int((sh - h) / 2)
+        x2 = sw - w
+        y2 = int((sh - h) / 2)
+        x = int(sw / 2) - 10
+        y = int(sh / 2)
+        V = False
+    elif x-r+ux < x1+w:
+        score2 += 1
+        x1 = 0
+        y1 = int((sh - h) / 2)
+        x2 = sw - w
+        y2 = int((sh - h) / 2)
+        x = int(sw / 2) - 10
+        y = int(sh / 2)
+        V = False
+    if y-r+uy < 0 and V:
         uy = -uy
-    elif y+r+uy > sh:
+    elif y+r+uy > sh and V:
         uy = -uy
+
 
     x += ux
     y += uy
     win.fill((0, 0, 0))
+    text1 = font.render(str(score1), 1, (255, 0, 0))
+    text2 = font.render(str(score2), 1, (255, 0, 0))
+    win.blit(text1, (100, 0))
+    win.blit(text2, (400, 0))
     pygame.draw.rect(win, (255, 255, 255), (x1, y1, w, h))
     pygame.draw.rect(win, (255, 255, 255), (x2, y2, w, h))
     pygame.draw.circle(win, (255, 255, 255), (x, y), r, 1)
     pygame.display.update()
+    if not V:
+        pygame.time.delay(1000)
+        V = True
+        ux = -ux
 
 pygame.quit()
