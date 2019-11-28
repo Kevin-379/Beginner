525c84c3ebcb39ba5e5731956fff74a212fa4c90
Kevin Shah <kevin2252238@gmail.com>
2019-11-28 15:20:34 +0530

    Learning pygame #2

--- Project1/learningPygame.py
+++ Project1/learningPygame.py
@@ -4,7 +4,9 @@ import pygame
 pygame.init()
 
 # creating a screen
-screen = pygame.display.set_mode((240, 180))
+sh = 240
+sw = 180
+screen = pygame.display.set_mode((sh, sw))
 
 # loading and setting logo and title
 #logo = pygame.image.load("logo32x32.png")
@@ -16,10 +18,13 @@ y = 50
 h = 20
 w = 20
 v = 5
+isJump = False
+T = 7
+t = T
 
 running = True
 while running:
-    pygame.time.delay(100)
+    pygame.time.delay(50)
 
     for event in pygame.event.get():
         # adding option to quit anytime
@@ -28,14 +33,24 @@ while running:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] and x >= v:
         x -= v
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and x <= sh-w-v:
         x += v
-    if keys[pygame.K_UP]:
-        y -= v
-    if keys[pygame.K_DOWN]:
-        y += v
+    if not isJump:
+        if keys[pygame.K_UP] and y >= v:
+            y -= v
+        if keys[pygame.K_DOWN] and y <= sw-h-v:
+            y += v
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if t >= -T:
+            y -= int(0.5*t*abs(t))
+            t -= 1
+        else:
+            t = T
+            isJump = False
 
     screen.fill((0, 0, 0))
     pygame.draw.rect(screen, (255, 0, 0), (x, y, h, w))
